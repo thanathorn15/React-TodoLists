cf2d145e4caa6f8e46163a17c8b80e4f1179fed4
Thanathorn15 <thanathorn_ko@kkumail.com>
2023-04-25 11:41:59 +0700

    react todo

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import { FaHome } from "react-icons/fa";
+export function Header() {
+  return (
+    <header className="header">
+      <span>
+        <FaHome />
+      </span>
+      <h3>CC-Todolist</h3>
+    </header>
+  );
+}
